39fef8d841e6e5065646e5d5c9dd865d1d07acb7
mhdfrhan <mhdfarhan5148@gmail.com>
2022-04-21 18:56:16 +0700

    commit kedua

--- public/js/script.js
+++ public/js/script.js
@@ -22,22 +22,34 @@ $(window).on('load', function () {
 	$('#loader').fadeOut(1000);
 });
 
-$(document).ready(function(){
-  $(".plus").click(function(){
-    var val= $(".product-qty").val();
-    val++;
-    if(val >= 1){
-      $(".product-qty").attr("value", val);
+$(document).ready(function() {
+  $('.quantity .flex-col div').click(function () {
+      var $input = $(this).parents('.quantity').find('.product-qty');
+    if($(this).hasClass('minus')) {
+      var count = parseFloat($input.val()) - 1;
+      count = count < 1 ? 1 : count;
+      if (count < 2) {
+        $(this).addClass('disabled');
+      }
+      else {
+        $(this).removeClass('disabled');
+      }
+      $input.val(count);
     }
-  });
-  $(".minus").click(function(){
-    var val= $(".product-qty").val();
-    val--;
-    if(val >= 1){
-      $(".product-qty").attr("value", val);
+    else {
+      var count = parseFloat($input.val()) + 1
+      $input.val(count);
+      if (count > 1) {
+        $(this).parents('.quantity').find(('.minus')).removeClass('disabled');
+      }
     }
+    
+    $input.change();
+    return false;
   });
+  
 });
+// product +/-
 
 
 document.addEventListener("DOMContentLoaded", function () {
